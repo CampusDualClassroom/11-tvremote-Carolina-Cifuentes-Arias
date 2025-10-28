9249d45584bd0f799c1836368b6816b6b68a7ce8
Carolina Cifuentes Arias <cifuentesariascarolina@gmail.com>
2025-10-28 19:23:39 +0100

    Solved Exercise 11

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -3,8 +3,42 @@ package com.campusdual.classroom;
 public class Exercise {
     public static void main(String[] args) {
         TVRemote redRemote = new TVRemote("Rojo");
-        TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
+
+        redRemote.turnOn();
+
+        redRemote.channelUp();
+        redRemote.channelDown();
+
+        redRemote.channelDown();
+
+        redRemote.volumeUp();
+        redRemote.volumeDown();
+
+        redRemote.volume = 0;
+        redRemote.volumeDown();
+
+        redRemote.turnOff();
+
+
+        TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(blackRemote.getColor());
+
+        blackRemote.turnOn();
+
+        blackRemote.channelUp();
+        blackRemote.channelDown();
+
+        blackRemote.channelDown();
+
+        blackRemote.volumeUp();
+        blackRemote.volumeDown();
+
+        blackRemote.volume = 0;
+        blackRemote.volumeDown();
+
+        blackRemote.turnOff();
+
+
     }
 }
